78f04022048d1f5a12f8f7314f5dec372d9a158a
Simon Situ <ssitu001@gmail.com>
2017-10-09 18:58:33 -0700

    add gradient

--- src/PlacesListItem.js
+++ src/PlacesListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './PlacesListItem.css';
+
 const PlacesListItem = ({place, index, displayInfoFromListItem}) => {
 
   return (
-    <div className="list-group-item" onClick={() => displayInfoFromListItem(index)}>
+    <div 
+      className={`list-group-item ${index % 2 === 0 ? 'list-group-item-alternate' : ''}`}
+      onClick={() => displayInfoFromListItem(index)}>
       <h5 className="list-group-item-heading">
         {place.name}
       </h5>
