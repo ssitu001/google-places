09b6d5e02234c0a56f73ffe756b4820ff15d09d4
Simon Situ <ssitu001@gmail.com>
2017-10-09 18:57:41 -0700

    refactor searchplacesinput to stateless functional

--- src/SearchPlacesInput.js
+++ src/SearchPlacesInput.js
@@ -1,46 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { ButtonGroup, Button, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-class SearchPlacesInput extends Component {
-  // constructor(props) {
-  //   super(props);
+const SearchPlacesInput = ({handleSubmit, handleChange}) => {
 
-  //   this.state = { place: '' };
-
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-  
-  // handleChange(e) {
-  //   this.setState({ place: e.target.value });
-  // }
-  
-  // handleSubmit(e) {
-  //   e.preventDefault();
-  // }
-  
-  render() {
-    // console.log('this.state', this.state)
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <form className="form-inline" onSubmit={this.props.handleSubmit}>
-              {/* <label>Search</label> */}
-              <input type="text"
-                className="form-control input-lg"
-                id="place"
-                placeholder="Search here..."
-                onChange={this.props.handleChange}
-                required/>
-                <button type="submit" className="btn btn-default btn-lg">
-                  <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
-                </button> 
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  return (
+    <Form inline onSubmit={handleSubmit}>
+      <FormGroup>
+        <FormControl 
+          type="text"
+          placeholder="Search Place Here..."
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+    {' '}
+      <Button type="submit">
+        Search
+      </Button>
+    </Form>
+  )
 }
 
 export default SearchPlacesInput;
