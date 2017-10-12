381cd88ec74cc79d5d42261dd8660709e448c40b
Simon Situ <ssitu001@gmail.com>
2017-10-11 17:29:02 -0700

    basic test place list item

--- src/Components/PlacesListItem/PlacesListItem.test.js
+++ src/Components/PlacesListItem/PlacesListItem.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import toJson from 'enzyme-to-json';
 import PlacesListItem from './PlacesListItem';
 
-xit('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PlacesListItem />, div);
+const place = {
+  name: 'Place1',
+  formatted_address: '123 Fake St',
+};
+
+test('renders PlacesListItem component as expected', () => {
+  const component = shallow(<PlacesListItem place={place}/>);
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});
+
+test('render PLaceListItem with alternate background', () => {
+  const component = shallow(<PlacesListItem place={place} index={0}/>);
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();  
+});
+
+test('render PLaceListItem with no background', () => {
+  const component = shallow(<PlacesListItem place={place} index={1}/>);
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();  
 });
