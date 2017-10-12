d38b611b94140de849285c8aa7c21a977d58aa06
Simon Situ <ssitu001@gmail.com>
2017-10-11 17:29:24 -0700

    basic test search places input

--- src/Components/SearchPlacesInput/SearchPlacesInput.test.js
+++ src/Components/SearchPlacesInput/SearchPlacesInput.test.js
@@ -6,5 +6,6 @@ import SearchPlacesInput from './SearchPlacesInput';
 test('renders SearchPlacesInput component as expected', () => {
   const component = shallow(<SearchPlacesInput />);
   const tree = toJson(component);
+
   expect(tree).toMatchSnapshot();
 });
